d4034fcfaa020eb39037c50e9e8509de00bc3f09
gabriel <gabriel.leal@roit.com.br>
2020-05-13 21:27:41 -0300

    adicionando o jogo de adivinhação

--- Jogos_de_adivinhacao/adivinhacao.py
+++ Jogos_de_adivinhacao/adivinhacao.py
@@ -1,1 +1,10 @@
-print("\n""ola gabriel")
+print("\n""ola gabriel, bem vindo ao jogo de adivinhação\n")
+
+numero_secreto = 18
+
+chute = int(input("digite o numero que voce deseja churtar:"))
+print("\nvoce chutor esse numero:",chute,"\n")
+if( chute == numero_secreto ):
+    print("parabens! voce acertou :)")
+else:
+    print("voce errou :(")
